6a72766e601c3b116177559436be1e97e791abb7
nufeng1999 <18478162@qq.com>
2021-12-12 14:19:25 +0800

    add setresolv

--- install.sh
+++ install.sh
@@ -25,6 +25,7 @@ ln -sf $PWD/wslpanel /usr/bin/wslpanel
 ln -sf $PWD/wslstartxfce4.sh /usr/bin/wslstartxfce4
 ln -sf $PWD/wslterminator.sh /usr/bin/wslterminator
 ln -sf $PWD/setwsl2bridge.sh  /usr/bin/setwsl2bridge
+ln -sf $PWD/setresolv.sh /usr/bin/setresolv.sh 
 
 chmod +x /usr/bin/setguienv
 chmod +x /usr/bin/setproxy
